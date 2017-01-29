282931c7a030a2b2355a025ee2557bfee7193855
pmendoza72 <pablo_mendoza@hotmail.com>
2017-01-28 17:05:23 -0800

    added new js question

--- app.js
+++ app.js
@@ -1,2 +1,90 @@
-var userName = prompt('Give me the name.');
-console.log('line 12: username');
+
+function myFunction() {
+
+  var userName = prompt ('What\'s your name?');
+  alert('Hello ' + userName + '. Thanks for coming to my site!')
+
+  // WE want to ask the user a question
+  // Take their answer as input; stored in a var
+  // Validate that their answer matches my answer
+  // Depending on True or False, provide feedback
+
+  var answerOne = prompt('What country was I born in? Hint: South America');
+  var myAnswerOne = 'CHILE';
+
+  if(answerOne.toUpperCase() === myAnswerOne) {
+    alert('Wow! You got it right. I was born in Chile.')
+    // Do something
+  } else {
+    alert('Sorry, that\'s wrong')
+    // Do something else
+  }
+
+  var answerTwo = prompt('What\'s my first language?');
+  var myAnswerTwo = 'SPANISH';
+
+  if(answerTwo.toUpperCase() === myAnswerTwo) {
+    alert('Si! My first language is Spanish.')
+    // Do something
+  } else {
+    alert('Sorry, that\'s wrong')
+    // Do something else
+  }
+
+  var answerThree = prompt('What is my favorite sport? Hint: pointy');
+  var myAnswerThree = 'FOOTBALL';
+
+  if(answerThree.toUpperCase() === myAnswerThree) {
+    alert('Correct! GO HAWKS!!!')
+    // Do something
+  } else {
+    alert('Sorry, that\'s wrong')
+    // Do something else
+  }
+
+  // User input must accept either y/n or yes/no responses
+  // With either .toUpperCase() or .toLowerCase() used to validate input
+
+  var answerFour = prompt('Is my favorite color blue?');
+
+  if(answerFour.toLowerCase() === 'yes' || answerFour.toLowerCase() === 'y') {
+    alert('You guessed right!')
+  } else {
+    alert('Sorry, that\'s wrong')
+  }
+
+  var myFavColor = prompt('What is my favorite color?');
+  var favColor = ['blue', 'green', 'red'];
+
+  if (favColor.indexOf(myFavColor.toLowerCase()) ===  -1) {
+    alert('Not close.')
+  } else if (favColor.indexOf(myFavColor.toLowerCase()) === 0) {
+    alert('You got it.')
+  } else {
+    alert('You are close.')
+  }
+
+
+
+
+  var userNumber;
+  var myNumber = 11;
+
+  do {
+
+      if ( userNumber ) {
+          if ( userNumber > myNumber ) {
+              alert( "Your guess was too high." );
+          }
+          else {
+              alert( "Your guess was too low." );
+          }
+      }
+
+      userNumber = prompt( "Guess my favorite number. It\'s from 1 to 100." );
+  } while ( userNumber != myNumber );
+
+  alert( "You guessed it!" );
+
+
+}
