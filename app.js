915e3dcdf5810a5b05797ec2a50b22b5e0cdcc04
pmendoza72 <pablo_mendoza@hotmail.com>
2017-02-03 01:28:33 -0800

    cleaning up js code. added more comments. used better name for variables

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ function myFunction() {
   // Ask and use the user's name
 
   var userName = prompt ('What\'s your name?');
-  alert('Hi ' + userName + '. Thanks for visiting my site!')
+  alert('Hi ' + userName + '. Let\'s play a guessing game!');
+  console.log('User\'s name is ' + userName + '.')
 
   // Guessing Game
   // We want to ask the user a question
@@ -14,67 +15,71 @@ function myFunction() {
 
   // Question #1
 
-  var answerOne = prompt('What country was I born in? Hint: South America');
+  var responseOne = prompt('Guess what country was I born in? Hint: South America');
   var myAnswerOne = 'CHILE';
 
-  if(answerOne.toUpperCase() === myAnswerOne) {
-    alert('Wow! You got it right. I was born in Chile.')
-    // Do something
+  if(responseOne.toUpperCase() === myAnswerOne) {
+    alert('Wow! You got it right. I was born in Chile.');
+    console.log(userName + '\'s guess of ' + responseOne + ' is correct!' )
   } else {
-    alert('Sorry, that\'s wrong')
-    // Do something else
+    alert('Sorry, that\'s wrong');
+    console.log(userName + '\'s guess of ' + responseOne + ' is wrong!' )
   }
 
   // Question #2
 
-  var answerTwo = prompt('What\'s my first language?');
+  var responseTwo = prompt('Guess my first language?');
   var myAnswerTwo = 'SPANISH';
 
-  if(answerTwo.toUpperCase() === myAnswerTwo) {
-    alert('Si! My first language is Spanish.')
-    // Do something
+  if(responseTwo.toUpperCase() === myAnswerTwo) {
+    alert('Si! My first language is Spanish. Great job ' + userName + '!')
+    console.log(userName + '\'s guess of ' + responseTwo + ' is correct.')
   } else {
     alert('Sorry, that\'s wrong')
-    // Do something else
+    console.log(userName + '\'s guess of ' + responseTwo + ' is wrong!' )
   }
 
   // Question #3
 
-  var answerThree = prompt('What is my favorite sport? Hint: pointy');
-  var myAnswerThree = 'FOOTBALL';
+  var responseThree = prompt('What is my favorite sport? Hint: pointy');
+  var myAnswerThree = 'football';
 
-  if(answerThree.toUpperCase() === myAnswerThree) {
-    alert('Correct! GO HAWKS!!!')
-    // Do something
+  if(responseThree.toLowerCase() === myAnswerThree) {
+    alert('That\'s correct! GO HAWKS!!!')
+    console.log(userName + ' guessed correctly that ' + myAnswerThree + ' is my favorite sport. GO HAWKS!!!')
   } else {
     alert('Sorry, that\'s wrong')
-    // Do something else
+        console.log(userName + '\'s guess of ' + responseThree + ' is wrong!' )
   }
 
+  // Question #4
+  // Logical operator
   // User input must accept either y/n or yes/no responses
   // With either .toUpperCase() or .toLowerCase() used to validate input
 
-  var answerFour = prompt('Is my favorite color blue?');
+  var responseFour = prompt('Do I have any children?');
 
-  if(answerFour.toLowerCase() === 'yes' || answerFour.toLowerCase() === 'y') {
-    alert('You guessed right!')
+  if(responseFour.toLowerCase() === 'yes' || responseFour.toLowerCase() === 'y') {
+    alert('You guessed right! I have one son who is 21 years old.')
   } else {
-    alert('Sorry, that\'s wrong')
+    alert('Sorry, that\'s wrong. I do have a son.')
   }
 
-  var myFavColor = prompt('What is my favorite color?');
+  // Question #5
+  // Multiple right answers
+
+  var responseFive = prompt('Name three of my favorite colors.');
   var favColor = ['blue', 'green', 'red'];
 
-  if (favColor.indexOf(myFavColor.toLowerCase()) ===  -1) {
-    alert('Not close.')
-  } else if (favColor.indexOf(myFavColor.toLowerCase()) === 0) {
-    alert('You got it.')
+  if (favColor.indexOf(responseFive.toLowerCase()) >  -1) {
+    alert('You got it right. ' + responseFive + ' is one of my favorite colors.')
   } else {
-    alert('You are close.')
+    alert('Sorry, ' + responseFive + ' is incorrect. Blue, green and red are my favorites.')
   }
 
-
-
+// Question #6
+// Numeric input and indicate whether the guess is
+// too high or too low
 
   var userNumber;
   var myNumber = 11;
