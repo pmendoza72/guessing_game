88578eeff2ad7c339aa3861f865acad6f85995a4
pmendoza72 <pablo_mendoza@hotmail.com>
2017-02-04 13:48:44 -0800

    finished question #7

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ function myFunction() {
 
   if(responseOne.toUpperCase() === myAnswerOne) {
     alert('Wow! You got it right. I was born in Chile.');
-    console.log(userName + '\'s guess is correct!' )
+    console.log(userName + '\'s guessed' + responseOne + ' correctly!' )
   } else {
     alert('Sorry, that\'s wrong. I was born in Chile.');
-    console.log(userName + '\'s guess is wrong!' )
+    console.log(userName + '\'s guess of ' + responseOne + ' is wrong!' )
   }
 
   // Question #2
@@ -32,10 +32,10 @@ function myFunction() {
 
   if(responseTwo.toUpperCase() === myAnswerTwo) {
     alert('Si! My first language is Spanish. Great job ' + userName + '!')
-    console.log(userName + '\'s guess is correct.')
+    console.log(userName + '\'s guess of Spanish.')
   } else {
     alert('Sorry, that\'s wrong. My first language is Spanish.')
-    console.log(userName + '\'s guess is wrong!' )
+    console.log(userName + '\'s guess of ' + responseTwo + ' is wrong!' )
   }
 
   // Question #3
@@ -60,7 +60,7 @@ function myFunction() {
 
   if(responseFour.toLowerCase() === 'yes' || responseFour.toLowerCase() === 'y') {
     alert('You guessed right! I have one son who is 21 years old.')
-    console.log(userName + ' guessed right.')
+    console.log(userName + ' guessed correctly that I have a child.')
   } else {
     alert('Sorry, that\'s wrong. I do have a son.')
     console.log(userName + ' guessed wrong.')
@@ -74,7 +74,7 @@ function myFunction() {
 
   if (favColor.indexOf(responseFive.toLowerCase()) >  -1) {
     alert('You got it right. It is one of my favorite colors.')
-    console.log(userName + ' guessed one of my favorite colors correctly.')
+    console.log(userName + ' guessed ' + responseFive + ' which is one of my favorite colors.')
   } else {
     alert('Sorry, ' + responseFive + ' is incorrect. Blue, green and red are my favorites.')
     console.log(userName + ' guessed wrong.')
@@ -89,22 +89,49 @@ function myFunction() {
 
   do {
     numberTries +=1
-    var userNumber = prompt( "Guess my favorite number. It\'s between 1 and 100." );
-      if ( userNumber > myNumber ) {
-        alert( "Your guess was too high." );
-      }
-      else if ( userNumber < myNumber ) {
-        alert( "Your guess was too low." );
-      }
-    } while ( userNumber != myNumber && numberTries < 4);
-
-    if (userNumber = myNumber) {
-      alert( "You guessed it!" );
-    } else {
-      alert('Sorry, no more tries.');
+    var userNumber = prompt( 'Guess my favorite number. It\'s between 1 and 100.' );
+    if ( userNumber > myNumber ) {
+      alert( 'Your guess was too high.' );
+    }
+    else if ( userNumber < myNumber ) {
+      alert( 'Your guess was too low.' );
     }
 
+  } while ( userNumber != myNumber && numberTries < 4);
+
+  if (userNumber == myNumber) {
+    alert( 'You guessed it!' );
+  } else {
+    alert('Sorry, no more tries.');
+  }
+
+  // Question #7
+  // Guessing game that accepts multiple correct answers stored in an array
+  // User has 6 tries
+  // Message to the user indicating all of the possible correct answers
+
+  var countryGuesses = 0;
+  var countriesVisited = [ 'chile' , 'brazil' , 'peru' , 'mexico' , 'canada'];
+  var countriesVisitedString = countriesVisited.join(" , ");
+
+  do {
+    countryGuesses +=1
+    var responseSeven = prompt('Guess which countries I have visited.');
+    if (countriesVisited.indexOf(responseSeven.toLowerCase()) !==  -1) {
+      console.log('This is where they got the right answer.')
+    }
+    else {
+      alert('Sorry. ' + responseSeven + ' is not a country I have visited.');
+    }
+
+  } while (countriesVisited.indexOf(responseSeven.toLowerCase()) === -1 && countryGuesses < 7);
+
+  if (countriesVisited.indexOf(responseSeven.toLowerCase()) === 0) {
+    alert('You guessed right! ' + countriesVisitedString + ' are all the countries I have visited.');
+  }  else {
+    alert('Sorry. You ran out of tries. ' + countriesVisitedString + ' are all the countries I have visited.');
+  }
 
 
 
-}
+  }
